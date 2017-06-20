ca982daa5d7c07e15eaf47cfffc3f6577a0c022e
Philip Putnam <putnamp989@gmail.com>
2017-06-20 13:10:02 -0700

    add custom middleware that alerts user to login if they want to view their profile

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 var mid = require('../middleware');
 
-router.get('/profile', function(req, res, next) {
-  if ( !req.session.userId) {
-    var err = new Error('You are not authorized to view this page');
-    err.status = 403;
-    return next(err);
-  }
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
   User.findById(req.session.userId)
     .exec(function (error, user) {
       if (error) {
